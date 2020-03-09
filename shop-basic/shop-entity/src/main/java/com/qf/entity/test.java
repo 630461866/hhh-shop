4cfb4a90823cfc8c06fd2efbfa075cb44fd73cd1
zhangjia <2505560247@qq.com>
2020-03-09 21:03:02 +0800

    这是测试代码

---
+++ shop-basic/shop-entity/src/main/java/com/qf/entity/test.java
@@ -0,0 +1,12 @@
+package com.qf.entity;
+
+/**
+ * @Author: zhangjia
+ * @Date: 2020/3/9 21:01
+ */
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("这是测试代码");
+    }
+}
