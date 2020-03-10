858ee03f2f85092f16c22b49abb3f7cf731d6e86
xcp <630461866@qq.com>
2020-03-10 13:49:02 +0800

    上交刚才漏下的

---
+++ shop-erueka-consumer/shop-index/src/test/java/com/qf/ShopIndexApplicationTests.java
@@ -0,0 +1,14 @@
+package com.qf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopIndexApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("快单给我提交啊啊啊");
+	}
+
+}
