b2805711a2294daf0daf34658063492d4072ca3b
zhangjia <2505560247@qq.com>
2020-03-21 10:16:47 +0800

    修改他们提交的代码2.0

---
+++ shop-erueka-consumer/shop-car/src/main/java/com/qf/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.qf.config;
+
+
+
+import com.qf.interceptor.AuthInterceptor1;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private AuthInterceptor1 authInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor).addPathPatterns("/**");
+    }
+}
